e4fcb01ab112cf30b94dafba8537d5064db65765
media-code-now <mediacodenow@gmail.com>
2022-02-06 12:47:21 -0800

    first commit

---
+++ js/bootstrap-toasts.init.js
@@ -0,0 +1,19 @@
+/*
+Template Name: artlink NFT Marketplace Template
+Author: Themesdesign
+Website: https://themesdesign.in/
+File: Bootstrap Toasts Js
+*/
+
+
+//  Bootstrap Toast 
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+
+        toast.show()
+    })
+}
+
